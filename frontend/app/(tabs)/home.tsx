f1890565afc74518882d024d585322416a5946f8
Najeeb Farhan <najeebfarhan6345@gmail.com>
2025-07-08 20:20:58 +0530

    home.tsx updated significantly

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -1,9 +1,53 @@
-import { Text, View } from "react-native";
+import { Tabs } from "expo-router";
+import { Bot, Camera, Home } from "lucide-react-native";
+import { useState } from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import WeatherWidget from '../components/weather_widget';
+import RateIcon from '../../assets/reshot-icon-money-GH3BJS8LUF.svg';
+import WeatherIcon from '../../assets/reshot-icon-sun-S9ZW4T6UGQ.svg';
 
-export default function Home() {
-    return (
-        <View className="flex-1 justify-center items-center">
-            <Text className="text-5xl font-bold">Home Page</Text>
-        </View>
-    )
-}
+
+export default function HomeScreen() {
+
+
+  return (
+    <SafeAreaView className="flex-1 bg-white px-4">
+      <View className="w-full flex-row justify-center items-center">
+      <View className="w-full flex-row justify-around items-center bg-transparent mt-5 mb-1">
+        <TouchableOpacity className="flex-grow bg-blue-500 p-4 rounded-l-xl flex-row justify-center items-center">
+          <WeatherIcon width={40} height={40}></WeatherIcon>
+          <Text className="text-center text-xl text-gray-100 font-bold mx-2">Weather</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-grow bg-green-400 p-4 rounded-r-xl flex-row justify-center items-center">
+          <RateIcon width={40} height={40}></RateIcon>
+          <Text className="text-center text-xl text-green-950 font-bold mx-2">Rate</Text>
+        </TouchableOpacity>
+      </View>
+      </View>
+      <ScrollView contentContainerStyle={{ alignItems: 'center', flexDirection: 'column', paddingVertical: 24 }}>
+          <WeatherWidget/>
+      </ScrollView>
+      <Tabs
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            if (route.name === "index") return <Home color={color} size={size} />;
+            if (route.name === "camera") return <Camera color={color} size={size === 24 ? 40 : size} />;
+            if (route.name === "chatbot") return <Bot color={color} size={size} />;
+            return null;
+          },
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            height: 70,
+            paddingBottom: 10,
+            paddingTop: 10,
+          },
+        })}
+      >
+        <Tabs.Screen name="index" options={{ title: "Home" }} />
+        <Tabs.Screen name="camera" options={{ title: "Camera" }} />
+        <Tabs.Screen name="chatbot" options={{ title: "Chatbot" }} />
+      </Tabs>
+    </SafeAreaView>
+  );
+} 
