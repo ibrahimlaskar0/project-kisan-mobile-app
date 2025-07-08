f7127835f2525194f630c82e59d8b5214f108fce
Najeeb Farhan <najeebfarhan6345@gmail.com>
2025-07-08 20:15:28 +0530

    added cache.ts for caching

---
+++ frontend/app/libs/cache.ts
@@ -0,0 +1,26 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const cacheWithExpiry = async (key: string, data: any, ttlseconds: number) => {
+  const item = {
+    value: data,
+    expiry: Date.now() + ttlseconds * 1000,
+  };
+  await AsyncStorage.setItem(key, JSON.stringify(item));
+};
+
+export const getCache = async (key: string) => {
+  const json = await AsyncStorage.getItem(key);
+  if (!json) return null;
+
+  try {
+    const item = JSON.parse(json);
+    if (Date.now() > item.expiry) {
+      await AsyncStorage.removeItem(key); // auto-clear expired cache
+      return null;
+    }
+    return item.value;
+  } catch (e) {
+    console.warn("Failed to parse cache", e);
+    return null;
+  }
+};
