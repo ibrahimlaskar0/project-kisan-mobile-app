9161eefb94b0c5da712f1eae067cd1aec2a410d1
Najeeb Farhan <najeebfarhan6345@gmail.com>
2025-07-08 15:55:13 +0530

    tabs for mobile apps has been made/planned, functionality will be added later

---
+++ frontend/app/(tabs)/home.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-5xl font-bold">Home Page</Text>
+        </View>
+    )
+}
