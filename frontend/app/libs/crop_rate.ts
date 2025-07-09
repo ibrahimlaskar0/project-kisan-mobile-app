e4d5ebbc3158deca42acbe9ef3394634acafc48e
Najeeb Farhan <najeebfarhan6345@gmail.com>
2025-07-09 19:16:32 +0530

    minor adjustments

--- frontend/app/libs/crop_rate.ts
+++ frontend/app/libs/crop_rate.ts
@@ -1,13 +1,14 @@
 type CropPriceRecord = {
-  state: string;
-  district: string;
-  market: string;
-  commodity: string;
-  variety: string;
-  arrival_date: string;
-  min_price: string;
-  max_price: string;
-  modal_price: string;
+  state?: string | null;
+  district?: string | null;
+  market?: string | null;
+  commodity?: string | null;
+  variety?: string | null;
+  arrival_date?: string | null;
+  min_price?: string | null;
+  max_price?: string | null;
+  modal_price?: string | null;
+  limit?: number
 };
 
 export async function fetchCropPrices({
@@ -15,22 +16,23 @@ export async function fetchCropPrices({
   state,
   district,
   limit = 10,
-}: {
+}: CropPriceRecord/*{
   commodity: string;
   state: string;
   district?: string;
   limit?: number;
-}): Promise<CropPriceRecord[] | null> {
+}*/): Promise<CropPriceRecord[] | null> {
   const API_KEY: string = process.env.EXPO_PUBLIC_GOV_API_KEY || "";
   const url = new URL("https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070");
 
-  url.searchParams.append("api-key", API_KEY);
+  url.searchParams.append("api-key", process.env.EXPO_PUBLIC_GOV_API_KEY || "");
   url.searchParams.append("format", "json");
   if (commodity) url.searchParams.append("filters[commodity]", commodity);
   if (state) url.searchParams.append("filters[state]", state);
   if (district) url.searchParams.append("filters[district]", district);
-  url.searchParams.append("limit", limit.toString());
+  if (limit) url.searchParams.append("limit", limit.toString());
 
+  console.log(url.toString())
   try {
     const res = await fetch(url.toString());
     const json = await res.json();
